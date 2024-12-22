7b3db8016cb1eb1a9407d67002121b9fdea3920f
victus-ubuntu-santy <santymc@icloud.com>
2024-12-22 01:22:24 +0000

    First commit of the proyect

---
+++ src/Spline.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Spline from '@splinetool/react-spline';
+
+export default function App() {
+  return (
+    <Spline scene="https://draft.spline.design/DWrfKv2ngc0nvzu4/scene.splinecode" />
+  );
+}
