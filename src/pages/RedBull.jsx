5a62b5c859708db2e9795c80ce981bbebd52a636
victus-ubuntu-santy <santymc@icloud.com>
2024-12-24 23:11:20 +0000

    Delete RoutePage and optimizate the code at the moment of the unlock

--- src/pages/RedBull.jsx
+++ src/pages/RedBull.jsx
@@ -18,48 +18,80 @@ function App() {
 
   function onLoadEvent(spline) {
     console.log('El objeto spline guardado es: ', spline)
+    console.log('El objeto parts guardado es: ', parts)
     setSplineRef(spline) // Guarda una referencia a la instancia de Spline
   }
 
   function onSplineMouseDownEvent(e) {
+    console.log("al seleccionar las partes son: ",parts)
     const selection = e.target.name;
 
     const elementoActivo = parts.find((p) => (p.parte == selection)).estado == false
     if(elementoActivo){
       setPartSelected(selection)
     } else {
-      return(
-        setAlert('El objeto ya fue desbloqueado')
-      )
+      setAlert('El objeto ya fue desbloqueado')
     }
   }
 
   function desbloquearParte(partName, codigo) {
+    if(parts.find((p) => p.parte == partName).codigo == codigo ) {
+      setParts(parts.map((p) => p.parte == partName ? {...p, estado: true} : p))
+      splineRef.setVariable(partName, true)
+      setSuccess(true)
+
+      // Actualizamos el nivel
+      let desbloqueados = parts.filter((p) => p.estado == true)
+      setLevel(desbloqueados.length)
+
+
+    } else {
+      setAlert('El codigo es incorrecto')
+    }
+
+    setPartSelected('')
+
+
+
+
+
+
+/*
     let auxParts = parts;
-    let part = auxParts.filter((p) => p.parte == partName)[0]
+
+    let part = auxParts.filter((p) => p.parte == partName)
+    if(part.length > 1){
+      part = part[0]
+    } 
+
     if(part) {
       if(part.codigo == codigo){
-        console.log(`La parte ${partName} ha sido desbloqueada`)
-        // Aca hay que actualizar el estado y hacer que se elimine
-        auxParts = auxParts.forEach(
-          (p) => { p.parte == partName ? p.estado = true : null})
+        // Aca hay que actualizar el estado y hacer que se elimine]
+        auxParts = auxParts.forEach((p) => {
+          if(p.parte == partName){
+            p.estado = true
+          }
+        })
         setParts(auxParts)
-        splineRef.setVariable(partName, true)
-        setPartSelected('')
-        return (
-          setSuccess(true)
-        )
-      } else {
-        setPartSelected('')
-        return (
-          setAlert('El codigo es incorrecto')
-        )
 
+        // Actualizamos la vista
+        splineRef.setVariable(partName, true)
+
+        // Deseleccionamos la parte
+        setSuccess(true)
+      } else {
+        setAlert('El codigo es incorrecto')
       }
+      setPartSelected('')
+
     }
+      */
   }
+
+
   useEffect(() => {
     if (success) {
+      setAlert(false);
       const timer = setTimeout(() => {
         setSuccess(false);
       }, 10000/2); 
@@ -70,6 +102,7 @@ function App() {
 
   useEffect(() => {
     if (alert) {
+      setSuccess(false);
       const timer = setTimeout(() => {
         setAlert(false);
       }, 10000/2); 
@@ -78,32 +111,23 @@ function App() {
     }
   }, [alert]);
 
-  useEffect(()=> {
-    // cambiar las bolas
-    if(parts){
-      let desbloqueados = parts.filter((p) => p.estado == true)
-      setLevel(desbloqueados.length)
-    } else {
-      setLevel(0)
-    }
-  }, [parts])
 
   return (
     <div className='relative h-screen w-screen'>
       <Suspense fallback={<div className='text-black'>Loading...</div>}>
         <div className="absolute top-10 left-1/2 transform -translate-x-1/2 flex gap-4 z-10">
           { 
-            level == 1 && <div className="w-16 h-16 bg-green-500 rounded-full"></div>
+            level === 1 && <div className="w-16 h-16 bg-green-500 rounded-full"></div>
           }
           {
-            level == 2 &&
+            level === 2 &&
             <>
               <div className="w-16 h-16 bg-green-500 rounded-full"></div>
               <div className="w-16 h-16 bg-green-500 rounded-full"></div>
             </>
           }
                   {
-            level == 3 &&
+            level === 3 &&
             <>
               <div className="w-16 h-16 bg-green-500 rounded-full"></div>
               <div className="w-16 h-16 bg-green-500 rounded-full"></div>
