5f55b95a9fe4b49100634f2f5a9c45cf7c944aed
victus-ubuntu-santy <santymc@icloud.com>
2024-12-25 00:12:03 +0000

    Cleaning code and adding comments

--- src/pages/RedBull.jsx
+++ src/pages/RedBull.jsx
@@ -17,75 +17,46 @@ function App() {
 
 
   function onLoadEvent(spline) {
-    console.log('El objeto spline guardado es: ', spline)
-    console.log('El objeto parts guardado es: ', parts)
     setSplineRef(spline) // Guarda una referencia a la instancia de Spline
   }
 
   function onSplineMouseDownEvent(e) {
-    console.log("al seleccionar las partes son: ",parts)
+    setPartSelected('')
     const selection = e.target.name;
-
-    const elementoActivo = parts.find((p) => (p.parte == selection)).estado == false
-    if(elementoActivo){
-      setPartSelected(selection)
+    console.log(selection)
+    let auxParts = parts
+    const part = auxParts.find((p) => p.parte == selection);
+    console.log(part)
+    if (part.estado === false) {
+      setPartSelected(selection);
     } else {
-      setAlert('El objeto ya fue desbloqueado')
+      setAlert('El objeto ya fue desbloqueado');
     }
+
   }
 
   function desbloquearParte(partName, codigo) {
-    if(parts.find((p) => p.parte == partName).codigo == codigo ) {
+    // Verificamos si el codigo es correcto
+    if(parts.find((p) => p.parte == partName).codigo == codigo) {
+      // Cambiamos el estado de la parte
       setParts(parts.map((p) => p.parte == partName ? {...p, estado: true} : p))
+
+      // Desbloqueamos el objeto en el spline
       splineRef.setVariable(partName, true)
+
+      // Mostramos el mensaje de exito
       setSuccess(true)
 
       // Actualizamos el nivel
-      let desbloqueados = parts.filter((p) => p.estado == true)
-      setLevel(desbloqueados.length)
+      const desbloqueados = parts.filter((p) => p.estado === true).length;
+      setLevel(desbloqueados + 1);
+      console.log("Nivel: ", desbloqueados)
 
 
     } else {
       setAlert('El codigo es incorrecto')
     }
-
     setPartSelected('')
-
-
-
-
-
-
-/*
-    let auxParts = parts;
-
-    let part = auxParts.filter((p) => p.parte == partName)
-    if(part.length > 1){
-      part = part[0]
-    } 
-
-    if(part) {
-      if(part.codigo == codigo){
-        // Aca hay que actualizar el estado y hacer que se elimine]
-        auxParts = auxParts.forEach((p) => {
-          if(p.parte == partName){
-            p.estado = true
-          }
-        })
-        setParts(auxParts)
-
-        // Actualizamos la vista
-        splineRef.setVariable(partName, true)
-
-        // Deseleccionamos la parte
-        setSuccess(true)
-      } else {
-        setAlert('El codigo es incorrecto')
-      }
-      setPartSelected('')
-
-    }
-      */
   }
 
 
@@ -115,19 +86,19 @@ function App() {
   return (
     <div className='relative h-screen w-screen'>
       <Suspense fallback={<div className='text-black'>Loading...</div>}>
-        <div className="absolute top-10 left-1/2 transform -translate-x-1/2 flex gap-4 z-10">
+        <div className=" absolute bottom-5 left-1/2 transform -translate-x-1/2 flex items-center justify-center w-10/12 gap-4 z-10">
           { 
-            level === 1 && <div className="w-16 h-16 bg-green-500 rounded-full"></div>
+            level == 1 && <div className="w-16 h-16 bg-green-500 rounded-full"></div> 
           }
           {
-            level === 2 &&
+            level == 2 &&
             <>
               <div className="w-16 h-16 bg-green-500 rounded-full"></div>
               <div className="w-16 h-16 bg-green-500 rounded-full"></div>
             </>
           }
                   {
-            level === 3 &&
+            level == 3 &&
             <>
               <div className="w-16 h-16 bg-green-500 rounded-full"></div>
               <div className="w-16 h-16 bg-green-500 rounded-full"></div>
@@ -145,14 +116,14 @@ function App() {
         
         {
           alert && 
-          <div className=' absolute top-0 left-1/2 transform -translate-x-1/2 flex items-center justify-center w-10/12' >
+          <div className=' absolute top-5 left-1/2 transform -translate-x-1/2 flex items-center justify-center w-10/12' >
             <Alert className='mt-4' strong="Error" text={alert}/>
           </div>
         }
 
         {
           success && 
-          <div className=' absolute top-0 left-1/2 transform -translate-x-1/2 flex items-center justify-center w-10/12' >
+          <div className=' absolute top-5 left-1/2 transform -translate-x-1/2 flex items-center justify-center w-10/12' >
             <Success className='mt-4' strong="Codigo Ingresado con Exito"/>
           </div>
         }
